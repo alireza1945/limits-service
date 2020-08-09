bf46e853fd55618367294b0c8ce9c1c18806963a
alireza1945 <alirezaazizollahi@gmail.com>
2020-08-09 19:59:36 +0430

    first limits-service is ok

--- src/main/java/ir/practice/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ src/main/java/ir/practice/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LimitsConfigurationController {
+public class  LimitsConfigurationController {
 
     private Configuration configuration;
 
